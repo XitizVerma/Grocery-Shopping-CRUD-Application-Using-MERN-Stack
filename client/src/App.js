3f3a935ce82b1102861a35e3ce860646205ab441
Xitiz Verma <xitizverma@gmail.com>
2020-12-17 03:25:06 +0530

    Added Create, Update and Delete Options1
    Updated CRUD Options and connected it to localhost connection with MySql WorkBench!

--- client/src/App.js
+++ client/src/App.js
@@ -1,21 +1,87 @@
+import React, { useState, useEffect } from 'react';
 import './App.css';
+import Axios from 'axios';
 
 function App() {
-  return (
-    <div className="App">
-     <h1>CRUD APPLICATION</h1>
-     <div class ="form">
-       <label>Movie Name</label>
-       <input type ="text" name ="Movie Name"/>
-       <label>Rating</label>
-       <input type ="text" name ="Rating"/>
-       <button type="submit">Submit</button>
-       
-     </div>
-     
-     
+  const [webseriesName, setwebseriesName] = useState('');
+  const [Rating, setwebseriesRating] = useState('');
+  const [webseriesReviewList, setwebseriesReview] = useState('');
+  const [newRating, sernewRating] = usestate('');
+
+  useEffect(() => {
+    Axios.get('https://localhost:3001/api/get').then((response) => {
+      setWebseriesList(response.data);
+    });
+  }, []);
+
+  const submitRating = () => {
+    Axios.post('https://localhost:3001/api/insert', {
+      webseriesName = webseriesName,
+      Rating = webseriesRating
+    });
+
+    setWebseriesList([
+      ...webseriesReviewList, {
+        webseriesName: webseriesName,
+        webseriesRating: Rating
+      },
+    ]);
+  };
+
+  const deleteRating = (webseries) => {
+    Axios.delete('https://localhost:3001/api/delete/ $ (webseries)');
+  };
+
+  const updateRating = (webseries) => { }
+  Axios.put('https://localhost:3001/api/update ', {
+    webseriesName = webseriesName,
+      Rating = webseriesRating,
+  });
+  setnewRating("")
+};
+
+
+return (
+  <div className="App">
+    <h1>CRUD APPLICATION</h1>
+
+    <div class="form">
+      <label>Webseries Name</label>
+      <input
+        type="text"
+        name="Webseries Name"
+        onChange={(e) => {
+          setwebseriesName(e.target.value)
+        }} />
+      <label>Rating</label>
+      <input
+        type="text"
+        name="Web Series Rating"
+        onChange={(e) => {
+          setwebseriesRating(e.target.value)
+        }} />
+
+      <button onClick={submitRating} type="submit">Submit</button>
+
+      {webseriesReviewList.localeCompare((val) => {
+        return (
+          <div className="card">
+            <h1>{val.webseriesName}</h1>
+            <p>{val.webseriesRating}</p>
+
+            <button onClick={deleteRating(val.webseriesName)}>Delete</button>
+
+            <input type="text" id="updateInput" onChange={(e) => {
+            setnewRating(e.target.value)
+            }}/>
+            <button onClick={() => {updateRating(val.webseriesName)}}>Update</button>
+          </div>
+        );
+
+      })}
+
     </div>
-  );
-}
+  </div>
+); 
 
 export default App;
--- server/index.js
+++ server/index.js
@@ -1,15 +1,71 @@
-const express = require("express"); 
-const app = express(); 
+const express = require("express");
+const app = express();
 const bodyParser = require("body-parser");
+const mysql = require("mysql");
+
+const connection = mysql.createConnection({
+  name: "localhost",
+  host: "127.0.0.1",
+  user: "root",
+  password: "12345678",
+  database: "cruddatabase",
+  port: 3306,
+  charset: "utf8mb4"
+});
+//npm run devStart to start server
+app.use(express.json);
+app.use(bodyParser.urlencoded({ extended: true }));
+
+
+connection.connect(err => {
+  if (!err) {
+    console.log("DB Connection Succeeded");
+  } else {
+    console.log("DB Connection Failed");
+  }
+});
+
+//displays message in command line
+//Webseries is the table name 
+//webseriesName and webseriesRating are the column attributes
+
+app.post("/api/insert", function (req, res) {
+
+  const webseriesName = req.body.webseriesName;
+  const webseriesRating = req.body.webseriesRating;
+
+
+  const InsertSql = "INSERT INTO webseriesreview (webseriesName , webseriesRating) VALUES ( 'My Love From the Star', 'Best Webseries');"
+  connection.query(InsertSql, [webseriesName, webseriesRating], (err, result) => {
+    console.log(result);
+  });
+});
+
+app.delete('/api/delete/:webseriesName',(req,res) => {
+  const name = req.params.webseriesName;
+  const Deletesql = "DELETE FROM webseriesreview WHERE webseriesName = ?";
+
+  connection.query(Deletesql,name,(err,result) => {
+    if (err)
+    console.log(err);
+  })
+})
+
+app.put('/api/update',(req,res) => {
+  const name = req.body.webseriesName;
+  const rating =req.body.webseriesRating;
+  const Updatetesql = "UPDATE webseriesreview SET webserieesRating = ? WHERE webseriesName =?";
+
+  connection.query(Updatetesql,[rating,name],(err,result) => {
+    if (err)
+    console.log(err);
+  })
+})
 
 
 
-app.get("/", function(req, res) { 
-    res.send("Thank You for calling!"); 
-}); 
+app.listen(3001, function () {
 
-app.listen(3001, function() {
-
-    console.log("Running on port 3001");
+  console.log("Running on port 3001");
 
 });
